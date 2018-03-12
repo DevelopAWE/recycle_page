cb1cb354a7380ccf14e0ce6980c0bb07965279dc
shortnd <ococncol@gmail.com>
2018-03-12 16:17:31 -0400

    added bootstrap and javascript, and started working on navbar

---
+++ app/Http/Controllers/RecyclePageController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RecyclePageController extends Controller
+{
+    public function index()
+    {
+        // return view('pages.index');
+        return view('pages.index');
+    }
+}
