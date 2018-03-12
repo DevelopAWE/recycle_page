5ad2798b57a3aa7b382c3509760201065faf7faa
shortnd <ococncol@gmail.com>
2018-03-12 16:32:49 -0400

    added a master blade file to extend in each page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,15 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-  <title>Recycle Index</title>
-</head>
-<body>
-  @include('components.navbar')
-  <h1>Index</h1>
-  <script src="{{ asset('js/app.js') }}"></script>
-</body>
-</html>
+@extends('components.master')
+
+@section('document-title')
+    Index
+@endsection
+
+@section('content')
+@endsection
