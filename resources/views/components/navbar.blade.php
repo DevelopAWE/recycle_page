7323240c860ebae24e5d30c63e15752eff1edb70
shortnd <ococncol@gmail.com>
2018-03-16 11:30:06 -0400

    heading/navbar and contact form styled up

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -68,54 +68,11 @@
             <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home menu-item-156" id="menu-item-156">
               <a href="#">Home</a>
             </li>
-            <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-380" id="menu-item-380">
-              <a href="#">Locations</a>
-              <ul class="sub-menu">
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-362" id="menu-item-362">
-                  <a href="#">Locations Page</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-369" id="menu-item-369">
-                  <a href="#">Fond Du Lac, WI</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-368" id="menu-item-368">
-                  <a href="#">Green Bay, WI</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-391" id="menu-item-391">
-                  <a href="#">La Vista, NE</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-367" id="menu-item-367">
-                  <a href="#">Lincoln, NE</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-366" id="menu-item-366">
-                  <a href="#">Manitowoc, WI</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-365" id="menu-item-365">
-                  <a href="#">Omaha, NE</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-364" id="menu-item-364">
-                  <a href="#">Oshkosh, WI</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-363" id="menu-item-363">
-                  <a href="#">Sheboygan, WI</a>
-                </li>
-              </ul>
-            </li>
             <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-162" id="menu-item-162">
               <a href="#">Why Wisconsin ITAD LLC</a>
             </li>
             <li class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-381" id="menu-item-381">
               <a href="#">About Us</a>
-              <ul class="sub-menu">
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-157" id="menu-item-157">
-                  <a href="#">About Us Page</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-382" id="menu-item-382">
-                  <a href="#">News</a>
-                </li>
-                <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-383" id="menu-item-383">
-                  <a href="#">Videos</a>
-                </li>
-              </ul>
             </li>
             <li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-158" id="menu-item-158">
               <a href="#">Contact</a>
@@ -150,7 +107,7 @@
             <div class="col-sm-12 text-white">
               <div class="wpcf7" id="wpcf7-f5-o1" role="form" lang="en-US" dir="ltr">
                 <div class="screen-reader-response"></div>
-                <form class="wpcf7-form" action="/electronics-recycling/#wpcf7-f5-o1" method="post" enctype="multipart/form-data" novalidate="novalidate">
+                <form class="wpcf7-form" action="" method="post">
                   <div style="display: none;">
                     <input name="_wpcf7" type="hidden" value="5">
                     <input name="_wpcf7_version" type="hidden" value="4.9.2">
@@ -163,12 +120,12 @@
                     <input name="_wpcf7cf_options" type="hidden" value="{&quot;form_id&quot;:5,&quot;conditions&quot;:[{&quot;if_field&quot;:&quot;fundraising-events&quot;,&quot;operator&quot;:&quot;equals&quot;,&quot;if_value&quot;:&quot;Yes&quot;,&quot;then_field&quot;:&quot;FundraisingEvents&quot;},{&quot;if_field&quot;:&quot;other&quot;,&quot;operator&quot;:&quot;equals&quot;,&quot;if_value&quot;:&quot;Yes&quot;,&quot;then_field&quot;:&quot;OtherGrp&quot;},{&quot;if_field&quot;:&quot;electronics-recycling-and-data-destruction&quot;,&quot;operator&quot;:&quot;equals&quot;,&quot;if_value&quot;:&quot;Yes&quot;,&quot;then_field&quot;:&quot;ElectronicsRecyclingDataDestruction&quot;},{&quot;if_field&quot;:&quot;scrap-metal-price-quotes&quot;,&quot;operator&quot;:&quot;equals&quot;,&quot;if_value&quot;:&quot;Yes&quot;,&quot;then_field&quot;:&quot;ScrapMetalPriceQuotes&quot;}],&quot;settings&quot;:{&quot;wpcf7cf_settings_saved&quot;:&quot;1&quot;,&quot;notice_dismissed&quot;:&quot;&quot;,&quot;animation&quot;:&quot;yes&quot;,&quot;animation_intime&quot;:&quot;200&quot;,&quot;animation_outtime&quot;:&quot;200&quot;}}">
                   </div>
                   <div class="row">
-                    <div class="col-sm-7">
+                    <div>
                       <p>
                         <strong>LET'S GET STARTED:</strong>
                       </p>
                       <div class="row">
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                           <label>
                             <span class="wpcf7-form-control-wrap your-name">
                               <input class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" name="your-name" type="text" aria-invalid="false"
@@ -190,7 +147,7 @@
                             </span>
                           </label>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-12">
                           <label>
                             <span class="wpcf7-form-control-wrap your-email">
                               <input class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" name="your-email"
@@ -209,205 +166,12 @@
                           <br>
                           <p>
                             <strong>GENERAL PHONE INQUIRIES
-                              <br>Wisconsin: (833) 373-2925 &nbsp; Nebraska: (402) 805-0065</strong>
+                              <br>Wisconsin: (262) 415-7443</strong>
                           </p>
                         </div>
-                        <div class="col-sm-12">
-                          <p>
-                            <strong>UPLOAD A SUPPORTING DOCUMENT</strong>
-                          </p>
-                        </div>
-                        <div class="col-sm-12">
-                          <span class="wpcf7-form-control-wrap fileupload">
-                            <input class="wpcf7-form-control wpcf7-file" name="fileupload" type="file" aria-invalid="false" size="40">
-                          </span>
-                        </div>
                       </div>
-                    </div>
-                    <div class="col-sm-5" id="interested-in__container">
-                      <p>
-                        <strong>YES! CONTACT ME REGARDING:*</strong>
-                      </p>
-                      <div class="scrap-quotes">
-                        <label>
-                          <span class="wpcf7-form-control-wrap scrap-metal-price-quotes">
-                            <span class="wpcf7-form-control wpcf7-checkbox">
-                              <span class="wpcf7-list-item first last">
-                                <input name="scrap-metal-price-quotes[]" type="checkbox" value="Yes">
-                                <span class="wpcf7-list-item-label">Yes</span>
-                              </span>
-                            </span>
-                          </span>
-                          <span class="radio-label">E-Recycling Material Quote Request</span>
-                        </label>
-                        <div id="ScrapMetalPriceQuotes" data-class="wpcf7cf_group">
-                          <div class="dropdown">YOUR ADDRESS:
-                            <span class="wpcf7-form-control-wrap street-quotes">
-                              <input class="wpcf7-form-control wpcf7-text" name="street-quotes" type="text" aria-invalid="false" value size="40" placeholder="STREET">
-                            </span>
-                            <span class="wpcf7-form-control-wrap city-quotes">
-                              <input class="wpcf7-form-control wpcf7-text" name="city-quotes" type="text" aria-invalid="false" value size="40" placeholder="CITY">
-                            </span>
-                            <div class="row">
-                              <div class="col-sm-6">
-                                <span class="wpcf7-form-control-wrap state-quotes">
-                                  <input class="wpcf7-form-control wpcf7-text" name="state-quotes" type="text" aria-invalid="false" value size="40" placeholder="STATE">
-                                </span>
-                              </div>
-                              <div class="col-sm-6">
-                                <span class="wpcf7-form-control-wrap zip-quotes">
-                                  <input class="wpcf7-form-control wpcf7-text" name="zip-quotes" type="text" aria-invalid="false" value size="40" placeholder="ZIP">
-                                </span>
-                              </div>
-                            </div>
-                            <p>
-                              <span class="wpcf7-form-control-wrap material-type-quotes">
-                                <input class="wpcf7-form-control wpcf7-text" name="material-type-quotes" type="text" aria-invalid="false" value size="40"
-                                  placeholder="MATERIAL TYPE">
-                              </span>
-                              <span class="wpcf7-form-control-wrap quantity-quotes">
-                                <input class="wpcf7-form-control wpcf7-text" name="quantity-quotes" type="text" aria-invalid="false" value size="40" placeholder="QUANTITY">
-                              </span>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="e-recycling">
-                        <label>
-                          <span class="wpcf7-form-control-wrap electronics-recycling-and-data-destruction">
-                            <span class="wpcf7-form-control wpcf7-checkbox">
-                              <span class="wpcf7-list-item first last">
-                                <input name="electronics-recycling-and-data-destruction[]" type="checkbox" value="Yes">
-                                <span class="wpcf7-list-item-label">Yes</span>
-                              </span>
-                            </span>
-                          </span>
-                          <span class="radio-label">Electronics Recycling & Data Destruction</span>
-                        </label>
-                        <div id="ElectronicsRecyclingDataDestruction" data-class="wpcf7cf_group">
-                          <div class="dropdown">YOUR ADDRESS:
-                            <span class="wpcf7-form-control-wrap street-erdd">
-                              <input class="wpcf7-form-control wpcf7-text" name="street-erdd" type="text" aria-invalid="false" value size="40" placeholder="STREET">
-                            </span>
-                            <span class="wpcf7-form-control-wrap city-erdd">
-                              <input class="wpcf7-form-control wpcf7-text" name="city-erdd" type="text" aria-invalid="false" value size="40" placeholder="CITY">
-                            </span>
-                            <div class="row">
-                              <div class="col-sm-6">
-                                <span class="wpcf7-form-control-wrap state-erdd">
-                                  <input class="wpcf7-form-control wpcf7-text" name="state-erdd" type="text" aria-invalid="false" value size="40" placeholder="STATE">
-                                </span>
-                              </div>
-                              <div class="col-sm-6">
-                                <span class="wpcf7-form-control-wrap zip-erdd">
-                                  <input class="wpcf7-form-control wpcf7-text" name="zip-erdd" type="text" aria-invalid="false" value size="40" placeholder="ZIP">
-                                </span>
-                              </div>
-                            </div>
-                            <p>
-                              <span class="wpcf7-form-control-wrap description-of-items">
-                                <textarea class="wpcf7-form-control wpcf7-textarea" name="description-of-items" aria-invalid="false" cols="40" rows="10"
-                                  placeholder="DESCRIPTION OF ITEMS YOU&#039;D LIKE TO RECYCLE"></textarea>
-                              </span>
-                              <span class="wpcf7-form-control-wrap quantity-erdd">
-                                <input class="wpcf7-form-control wpcf7-text" name="quantity-erdd" type="text" aria-invalid="false" value size="40" placeholder="QUANTITY">
-                              </span>
-                            </p>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="fundraising">
-                        <label>
-                          <span class="wpcf7-form-control-wrap fundraising-events">
-                            <span class="wpcf7-form-control wpcf7-checkbox">
-                              <span class="wpcf7-list-item first last">
-                                <input name="fundraising-events[]" type="checkbox" value="Yes">
-                                <span class="wpcf7-list-item-label">Yes</span>
-                              </span>
-                            </span>
-                          </span>
-                          <span class="radio-label">Fundraising Events</span>
-                        </label>
-                        <div id="FundraisingEvents" data-class="wpcf7cf_group"></div>
-                      </div>
-                      <div class="other">
-                        <label>
-                          <span class="wpcf7-form-control-wrap other">
-                            <span class="wpcf7-form-control wpcf7-checkbox">
-                              <span class="wpcf7-list-item first last">
-                                <label>
-                                  <input name="other[]" type="checkbox" value="Yes">
-                                  <span class="wpcf7-list-item-label">Yes</span>
-                                </label>
-                              </span>
-                            </span>
-                          </span>
-                          <span class="radio-label">Other</span>
-                        </label>
-                        <div id="OtherGrp" data-class="wpcf7cf_group"></div>
-                      </div>
-                      <div class="careers">Click
-                        <a href="#">here</a> if you are interested in a career with Sadoff.</div>
-                      <div class="interested-validation"></div>
-                    </div>
-                    <div class="hidden" id="contact-forward">
-                      <span class="wpcf7-form-control-wrap contact-list">
-                        <span class="wpcf7-form-control wpcf7-checkbox wpcf7-validates-as-required">
-                          <span class="wpcf7-list-item first">
-                            <input name="contact-list[]" type="checkbox" value="DISPATCH">
-                            <span class="wpcf7-list-item-label">DISPATCH</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="LASKYJ">
-                            <span class="wpcf7-list-item-label">LASKYJ</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="EPPL">
-                            <span class="wpcf7-list-item-label">EPPL</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="LASKYM">
-                            <span class="wpcf7-list-item-label">LASKYM</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="STATHC">
-                            <span class="wpcf7-list-item-label">STATHC</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="BERGLUNDM">
-                            <span class="wpcf7-list-item-label">BERGLUNDM</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="KATZM">
-                            <span class="wpcf7-list-item-label">KATZM</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="STAEBELLA">
-                            <span class="wpcf7-list-item-label">STAEBELLA</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="HAYESC">
-                            <span class="wpcf7-list-item-label">HAYESC</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="CIESZYNSKIJ">
-                            <span class="wpcf7-list-item-label">CIESZYNSKIJ</span>
-                          </span>
-                          <span class="wpcf7-list-item">
-                            <input name="contact-list[]" type="checkbox" value="WEIRMANM">
-                            <span class="wpcf7-list-item-label">WEIRMANM</span>
-                          </span>
-                          <span class="wpcf7-list-item last">
-                            <input name="contact-list[]" type="checkbox" value="INFO">
-                            <span class="wpcf7-list-item-label">INFO</span>
-                          </span>
-                        </span>
-                      </span>
                     </div>
                   </div>
-                  <p>
-                    <br>
-                  </p>
                   <div class="row">
                     <div class="col-xs-12">
                       <input class="wpcf7-form-control wpcf7-submit" type="submit" value="SUBMIT">
@@ -424,5 +188,4 @@
           </div>
         </div>
       </div>
-
     </header>
